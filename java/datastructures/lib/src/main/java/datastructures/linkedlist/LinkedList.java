bd5f81aa09b0ffe22b3426ed8041b02fc6058e10
DaJon Ewing <dajone37@gmail.com>
2023-04-24 20:11:09 -0700

    cc06

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -1,17 +1,86 @@
 package datastructures.linkedlist;
 
-public class LinkedList
-{
+public class LinkedList {
   public Node head;
 
   public LinkedList() {
     this.head = null;
   }
 
+
   public void insert(int value) {
     Node newNode = new Node(value);
     newNode.next = head;
     head = newNode;
+  }
+
+  public void append(int value) {
+    Node newNode = new Node(value);
+
+    if (this.head == null) {
+      this.head = newNode;
+      return;
+    }
+    Node current = this.head;
+
+    while (current.next != null) {
+      current = current.next;
+    }
+    current.next = newNode;
+
+  }
+
+  public boolean insertBefore(int valueToInsertBefore, int newValue) {
+    Node newNode = new Node(newValue);
+
+    if (this.head == null) {
+      return false;
+    }
+
+    Node current = this.head;
+
+    if (current.value == valueToInsertBefore) {
+      insert(newValue);
+      return true;
+    }
+
+    while (current.next != null) {
+      if (current.next.value == valueToInsertBefore) {
+        newNode.next = current.next;
+        current.next = newNode;
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+
+    return false;
+  }
+
+
+  public boolean insertAfter(int valueToInsertAfter, int newValue) {
+    Node newNode = new Node(newValue);
+
+    if (this.head == null) {
+      return false;
+    }
+
+    if (this.head.value == valueToInsertAfter) {
+      this.append(newValue);
+    }
+
+    Node current = this.head;
+
+    while (current != null) {
+      if (current.value == valueToInsertAfter) {
+        newNode.next = current.next;
+        current.next = newNode;
+        return true;
+      }
+      current = current.next;
+    }
+
+    return false;
   }
 
   public boolean includes(int value) {
@@ -24,18 +93,18 @@ public class LinkedList
     }
     return false;
   }
+
   @Override
-  public String toString()
-   {
-     StringBuilder sb = new StringBuilder();
-     Node current = head;
-     while (current != null) {
-       sb.append("{");
-       sb.append(current.value);
-       sb.append(" } -> ");
-       current = current.next;
-     }
-     sb.append("NULL");
-       return sb.toString();
+  public String toString() {
+    {
+      Node current = this.head;
+      String values = "";
+      while (current != null) {
+        values += String.format("{ %d } -> ", current.value);
+        current = current.next;
+      }
+      values += "NULL";
+      return values;
     }
+  }
 }
